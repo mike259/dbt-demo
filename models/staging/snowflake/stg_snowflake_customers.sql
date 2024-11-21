2321ecf1d75b55a28dd8c567d75b02213ab7d713
Mike <michael.lydon@avito.no>
2024-11-21 13:00:12 +0000

    updated sources

--- models/staging/snowflake/stg_snowflake_customers.sql
+++ models/staging/snowflake/stg_snowflake_customers.sql
@@ -2,11 +2,11 @@ with customers as (
 
     select
         id as customer_id,
-        first_name,
-        last_name
+        split_part(name,' ',1) as first_name,
+        split_part(name,' ',-1) as last_name
 
     from raw.jaffle_shop.customers
 
-),
+)
 
 select * from customers
